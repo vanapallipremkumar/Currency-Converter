716049c033b2311543f38ce0106fdd709e0b3100
vanapallipremkumar <vanapallipremkumar@gmail.com>
2021-09-23 06:45:33 +0000

    Added country details

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,32 @@
+import {Component} from 'react'
 import Cookies from 'js-cookie'
 
-const Home = props => {
-  if (Cookies.get('cy_jwt_token') === undefined) {
-    const {history} = props
-    history.replace('/signin')
-  }
-  const onclicksignout = () => {
+import countries from '../../countries'
+import './index.css'
+
+class Home extends Component {
+  state = {countryCode: countries[0].countryCode}
+
+  onclickLogout = () => {
     Cookies.remove('cy_jwt_token')
-    const {history} = props
+    const {history} = this.props
     history.replace('/signin')
   }
-  return (
-    <button type="button" onClick={onclicksignout}>
-      Logout
-    </button>
-  )
+
+  getCountryDetails = currencyCode =>
+    countries.find(country => country.currencyCode === currencyCode)
+
+  renderOptionsBar = () => {}
+
+  render() {
+    if (Cookies.get('cy_jwt_token') === undefined) {
+      const {history} = this.props
+      history.replace('/signin')
+    }
+
+    // JSX
+    return <div className="home-page-container">{this.renderOptionsBar()}</div>
+  }
 }
 
 export default Home
