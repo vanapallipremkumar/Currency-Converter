7d328442346c50a9b2d59bb49eb833e6ea266cb5
vanapallipremkumar <vanapallipremkumar@gmail.com>
2021-09-23 07:37:59 +0000

    Added select options to select countroes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,8 +4,16 @@ import Cookies from 'js-cookie'
 import countries from '../../countries'
 import './index.css'
 
+const emptyCountryDetails = {
+  countryCode: '',
+  countryName: '',
+}
+
 class Home extends Component {
-  state = {countryCode: countries[0].countryCode}
+  state = {
+    activeFromCountry: countries[0],
+    activeToCountry: {...emptyCountryDetails},
+  }
 
   onclickLogout = () => {
     Cookies.remove('cy_jwt_token')
@@ -13,10 +21,72 @@ class Home extends Component {
     history.replace('/signin')
   }
 
-  getCountryDetails = currencyCode =>
-    countries.find(country => country.currencyCode === currencyCode)
+  onChangeFromCountry = event => {
+    const countryCode = event.target.value
+    const activeFromCountry = countries.find(
+      country => country.countryCode === countryCode,
+    )
+    this.setState({activeFromCountry})
+  }
 
-  renderOptionsBar = () => {}
+  onChangeToCountry = event => {
+    const countryCode = event.target.value
+    if (countryCode === '') {
+      this.setState({activeToCountry: {...emptyCountryDetails}})
+    }
+  }
+
+  renderOptionsBar = () => {
+    const {activeFromCountry, activeToCountry} = this.state
+    return (
+      <div className="options-selector-container">
+        <h1 className="page-heading">Currency Converter</h1>
+
+        <div className="select-options-container">
+          <label className="select-label" htmlFor="fromCountry">
+            From
+          </label>
+          <select
+            className="select-menu"
+            value={activeFromCountry.countryName}
+            id="fromCountry"
+            onChange={this.onChangeFromCountry}
+          >
+            {countries.map(country => {
+              const {countryCode, countryName} = country
+              return (
+                <option value={countryCode} key={countryCode}>
+                  {countryName}
+                </option>
+              )
+            })}
+          </select>
+        </div>
+
+        <div className="select-options-container">
+          <label className="select-label" htmlFor="toCountry">
+            To
+          </label>
+          <select
+            className="select-menu"
+            value={activeToCountry.countryName}
+            id="toCountry"
+            onChange={this.onChangeToCountry}
+          >
+            <option value="">Choose Country</option>
+            {countries.map(country => {
+              const {countryCode, countryName} = country
+              return (
+                <option value={countryCode} key={countryCode}>
+                  {countryName}
+                </option>
+              )
+            })}
+          </select>
+        </div>
+      </div>
+    )
+  }
 
   render() {
     if (Cookies.get('cy_jwt_token') === undefined) {
