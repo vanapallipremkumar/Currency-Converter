1e5e1fdae05ab3defce86031c131d4b7cabe63a6
vanapallipremkumar <vanapallipremkumar@gmail.com>
2021-09-24 08:15:17 +0000

    Succeeded retriving data while fetching

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,78 @@
 import {Component} from 'react'
 import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/ThreeDots'
+import {BiError} from 'react-icons/bi'
+
+import SelectCountries from '../SelectCountries'
 
 import countries from '../../countries'
 import './index.css'
 
-const emptyCountryDetails = {
-  countryCode: '',
-  countryName: '',
+const status = {
+  success: 'SUCCESS',
+  loading: 'LOADING',
+  failed: 'FAILED',
 }
 
 class Home extends Component {
   state = {
-    activeFromCountry: countries[0],
-    activeToCountry: {...emptyCountryDetails},
+    convertFromCountry: countries[0].countryCode,
+    convertToCountry: 'all',
+    amount: 1,
+    pageStatus: status.loading,
+    countriesAmounts: [],
+  }
+
+  componentDidMount() {
+    this.loadData()
+  }
+
+  getApiUrl = () => {
+    const apiKey = 'd2e6d93cfd1f547eb5b8b2cd'
+    const {convertFromCountry, convertToCountry} = this.state
+    if (convertToCountry !== 'all') {
+      return `https://v6.exchangerate-api.com/v6/${apiKey}/pair/EUR/GBP`
+    }
+    return `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${convertFromCountry}`
+  }
+
+  onSuccessfulFetch = data => {
+    this.setState({pageStatus: status.success})
+    const {convertToCountry, amount} = this.state
+    let newData
+    if (convertToCountry !== 'all') {
+      newData = [
+        {
+          conversionRate: data.conversion_rate * amount,
+          targetCountryCode: data.target_code,
+        },
+      ]
+    } else {
+      const conversionRates = data.conversion_rates
+      newData = countries.map(country => {
+        const {countryCode} = country
+        const rate = conversionRates[countryCode]
+        return {
+          conversionRate: rate * amount,
+          targetCountryCode: countryCode,
+        }
+      })
+    }
+    console.log(newData)
+  }
+
+  loadData = async () => {
+    const apiUrl = this.getApiUrl()
+    console.log(apiUrl)
+    const options = {method: 'GET'}
+    const response = await fetch(apiUrl, options)
+    if (response.ok) {
+      const data = await response.json()
+      this.onSuccessfulFetch(data)
+    } else {
+      this.setState({pageStatus: status.failed})
+    }
   }
 
   onclickLogout = () => {
@@ -21,81 +81,77 @@ class Home extends Component {
     history.replace('/signin')
   }
 
+  getCountryDetailsByCode = countryCode =>
+    countries.find(country => country.countryCode === countryCode)
+
   onChangeFromCountry = event => {
     const countryCode = event.target.value
-    const activeFromCountry = countries.find(
-      country => country.countryCode === countryCode,
-    )
-    this.setState({activeFromCountry})
+    const convertFromCountry = this.getCountryDetailsByCode(countryCode)
+    this.setState({convertFromCountry: convertFromCountry.countryCode})
   }
 
   onChangeToCountry = event => {
-    const countryCode = event.target.value
-    if (countryCode === '') {
-      this.setState({activeToCountry: {...emptyCountryDetails}})
+    this.setState({convertToCountry: event.target.value})
+  }
+
+  onChangeAmount = event => this.setState({amount: event.target.value})
+
+  onClickConvert = () =>
+    this.setState({pageStatus: status.loading}, this.loadData)
+
+  renderCountriesAmountsPage = () => {
+    const {pageStatus} = this.state
+    switch (pageStatus) {
+      case status.success:
+        return this.renderCountryAmounts()
+      case status.loading:
+        return this.renderLoadingPage()
+      case status.failed:
+        return this.renderFailedPage()
+      default:
+        break
     }
+    return null
   }
 
-  renderOptionsBar = () => {
-    const {activeFromCountry, activeToCountry} = this.state
-    return (
-      <div className="options-selector-container">
-        <h1 className="page-heading">Currency Converter</h1>
+  renderLoadingPage = () => (
+    <div className="result-container loading-or-failed">
+      <Loader type="ThreeDots" color="#008080" />
+    </div>
+  )
 
-        <div className="select-options-container">
-          <label className="select-label" htmlFor="fromCountry">
-            From
-          </label>
-          <select
-            className="select-menu"
-            value={activeFromCountry.countryName}
-            id="fromCountry"
-            onChange={this.onChangeFromCountry}
-          >
-            {countries.map(country => {
-              const {countryCode, countryName} = country
-              return (
-                <option value={countryCode} key={countryCode}>
-                  {countryName}
-                </option>
-              )
-            })}
-          </select>
-        </div>
+  renderFailedPage = () => (
+    <div className="result-container loading-or-failed">
+      <BiError color="#ff0000aa" size="80" />
+      <h1 className="failed-message">Something went wrong</h1>
+      <p className="failed-message-description">
+        Try after changing the api-key
+      </p>
+    </div>
+  )
 
-        <div className="select-options-container">
-          <label className="select-label" htmlFor="toCountry">
-            To
-          </label>
-          <select
-            className="select-menu"
-            value={activeToCountry.countryName}
-            id="toCountry"
-            onChange={this.onChangeToCountry}
-          >
-            <option value="">Choose Country</option>
-            {countries.map(country => {
-              const {countryCode, countryName} = country
-              return (
-                <option value={countryCode} key={countryCode}>
-                  {countryName}
-                </option>
-              )
-            })}
-          </select>
-        </div>
-      </div>
-    )
-  }
+  renderCountryAmounts = () => null
 
   render() {
+    const {amount} = this.state
     if (Cookies.get('cy_jwt_token') === undefined) {
       const {history} = this.props
       history.replace('/signin')
     }
 
     // JSX
-    return <div className="home-page-container">{this.renderOptionsBar()}</div>
+    return (
+      <div className="home-page-container">
+        <SelectCountries
+          amount={amount}
+          onChangeFromCountry={this.onChangeFromCountry}
+          onChangeToCountry={this.onChangeToCountry}
+          onChangeAmount={this.onChangeAmount}
+          onClickConvert={this.onClickConvert}
+        />
+        {this.renderCountriesAmountsPage()}
+      </div>
+    )
   }
 }
 
---
+++ src/components/SelectCountries/index.js
@@ -0,0 +1,75 @@
+import countries from '../../countries'
+import './index.css'
+
+const Countries = props => {
+  const {
+    amount,
+    onChangeFromCountry,
+    onChangeToCountry,
+    onChangeAmount,
+    onClickConvert,
+  } = props
+  return (
+    <div className="options-selector-container">
+      <h1 className="page-heading">Currency Converter</h1>
+
+      <div className="inputs-container">
+        <label className="label" htmlFor="fromCountry">
+          From
+        </label>
+        <select
+          className="select-menu"
+          id="fromCountry"
+          onChange={onChangeFromCountry}
+        >
+          {countries.map(country => {
+            const {countryCode, countryName} = country
+            return (
+              <option className="option" value={countryCode} key={countryCode}>
+                {countryName}
+              </option>
+            )
+          })}
+        </select>
+      </div>
+
+      <div className="inputs-container">
+        <label className="label" htmlFor="toCountry">
+          To
+        </label>
+        <select
+          className="select-menu"
+          id="toCountry"
+          onChange={onChangeToCountry}
+        >
+          <option value="all">All Countries</option>
+          {countries.map(country => {
+            const {countryCode, countryName} = country
+            return (
+              <option value={countryCode} key={countryCode}>
+                {countryName}
+              </option>
+            )
+          })}
+        </select>
+      </div>
+
+      <div className="inputs-container">
+        <label className="label" htmlFor="toCountry">
+          Amount
+        </label>
+        <input
+          className="amount-input"
+          type="number"
+          onChange={onChangeAmount}
+          defaultValue={amount}
+        />
+      </div>
+      <button className="convert-button" type="button" onClick={onClickConvert}>
+        Convert
+      </button>
+    </div>
+  )
+}
+
+export default Countries
